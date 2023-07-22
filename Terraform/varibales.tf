dac6e0b4d4a45ea3c577d2ca669da30659093edf
ardin <ardindd@gmail.com>
2023-07-22 09:49:12 +0200

    aws

--- Terraform/varibales.tf
+++ Terraform/varibales.tf
@@ -1,18 +1,10 @@
 
-variable "aws_access_key" {
+variable "access_key" {
   type        = string
   description = "AWS Access Key"
 }
 
-variable "aws_secret_key" {
+variable "secret_key" {
   type        = string
   description = "AWS Secret Key"
-}
-
-variable "argocd_username" {
-  type        = string
-}
-
-variable "argocd_password" {
-  type        = string
 }
